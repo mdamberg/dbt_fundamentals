8bd1984bbdc141a42d646b9aefe0de9f7efa8178
DataDad <mattdamberg@gmail.com>
2025-11-21 13:43:12 -0600

    added fct_orders model

---
+++ models/marts/Finance/fct_orders.sql
@@ -0,0 +1,10 @@
+select
+    so.order_id,
+    sc.customer_id,
+    p.amount
+from {{ ref('stg_jaffle_shop_orders') }} so
+join {{ ref('stg_jaflle_shop_customers') }} sc 
+    on so.customer_id = sc.customer_id
+join {{ ref('stg_stripe_payments') }} p
+    on so.order_id = p.order_id
+    
---
+++ models/staging/Stripe/stg_stripe_payments.sql
@@ -0,0 +1,9 @@
+
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    round(amount / 100, 2) as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
